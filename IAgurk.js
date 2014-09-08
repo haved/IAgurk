69e07fbd38de608fe3804de50dd697923a2c605c
haved <krogstie.havard@gmail.com>
2014-09-08 17:12:17 +0000

    Commit from the CommitAllAndPush.sh

--- IAgurk.js
+++ IAgurk.js
@@ -3,27 +3,65 @@ function addFood(name, data)
 {
 	var food = new Object();
 	food["name"] = name;
-	
-	foods.append();
+	food["data"] = data;
+	foods[foods.length] = food;
 }
 
-addFood();
+function makeData(name, metric)
+{
+	var o = new Object();
+	o["name"] = name;
+	o["metric"] = metric;
+	return o;
+}
+
+addFood("Cucumber", [makeData("unit", 1), makeData("cm", 30)]);
+
 
 function fillLists()
 {
 	var options;
-	for()
-	var name = "Cucumber"
-	options = "<option id='Food"+name+"'value='Food"+name+"'>" + name + "</options>"
+	for(var i = 0; i < foods.length; i++)
+	{
+		var name = foods[i]["name"];
+		options += "<option id='Food"+name+"'value='"+name+"'>" + name + "</options>"
+	}
 	document.getElementById("foodSelect").innerHTML=options;
+	
+	updateValueLists();
+}
+
+function updateValueLists()
+{
+	var options;
+	var selection = document.getElementById("foodSelect").selectedIndex;
+	for(var j = 0; j < foods[selection]["data"].length; j++)
+	{
+		var name = foods[selection]["data"][j]["name"];
+		options += "<option id='metric_"+name+"'value='"+name+"'>" + name + "</options>"
+	}
+	document.getElementById("metricSelect1").innerHTML=options;
+	document.getElementById("metricSelect2").innerHTML=options;
 }
 
 function calc()
 {
+	var valueIn = parseFloat(document.getElementById("valueNumber1").value);
+	var food = foods[document.getElementById("foodSelect").selectedIndex];
+	var valueTypeIn = food["data"][document.getElementById("metricSelect1").selectedIndex];
+	var valueTypeOut = food["data"][document.getElementById("metricSelect2").selectedIndex];
 	
+	document.getElementById("valueNumber2").value = valueIn / valueTypeIn["metric"] * valueTypeOut["metric"];
 }
 
 function switc()
 {
+	var val1 = document.getElementById("valueNumber1").value;
+	var valType1 = document.getElementById("metricSelect1").selectedIndex;
 	
+	document.getElementById("valueNumber1").value = document.getElementById("valueNumber2").value;
+	document.getElementById("metricSelect1").selectedIndex = document.getElementById("metricSelect2").selectedIndex;
+	
+	document.getElementById("valueNumber2").value = val1;
+	document.getElementById("metricSelect2").selectedIndex = valType1;
 }
--- IAgurkLANG.js
+++ IAgurkLANG.js
@@ -1,17 +1,21 @@
 function localizeNN()
 {
-	document.getElementById("siteTitle").innerHTML="IAgurk";
-	document.getElementById("title").innerHTML="IAgurk";
+	document.getElementById("siteTitle").innerHTML="iAgurk";
+	document.getElementById("title").innerHTML="iAgurk";
 	document.getElementById("calcButton").innerHTML="regn ut";
 	document.getElementById("switchButton").innerHTML="bytt";
 	document.getElementById("FoodCucumber").innerHTML="agurk";
+	document.getElementById("metric_unit").innerHTML="stykk";
+	document.getElementById("metric_cm").innerHTML="cm";
 }
 
 function localizeEN()
 {
-	document.getElementById("siteTitle").innerHTML="ICucumber";
-	document.getElementById("title").innerHTML="ICucumber";
+	document.getElementById("siteTitle").innerHTML="iCucumber";
+	document.getElementById("title").innerHTML="iCucumber";
 	document.getElementById("calcButton").innerHTML="calc";
 	document.getElementById("switchButton").innerHTML="switch";
 	document.getElementById("FoodCucumber").innerHTML="cucumber";
+	document.getElementById("metric_unit").innerHTML="unit";
+	document.getElementById("metric_cm").innerHTML="cm";
 }
